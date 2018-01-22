f8993cd3ab6e53c38e56124bf96a6e39fa40245c
linxinzhe <linxinzhechn@icloud.com>
2018-01-22 17:24:49 +0800

    feat(object): anonymous interface

---
+++ object/anonymous_interface_test.go
@@ -0,0 +1,40 @@
+package object
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+type Student struct {
+	Human // 匿名字段，那么默认Student就包含了Human的所有字段
+	speciality string
+}
+
+func TestAnonymousInterface(t *testing.T) {
+	// 我们初始化一个学生
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
+
+	// 我们访问相应的字段
+	fmt.Println("His name is ", mark.name) //这里就当作自己的属性一样调用
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("His weight is ", mark.weight)
+	fmt.Println("His speciality is ", mark.speciality)
+	// 修改对应的备注信息
+	mark.speciality = "AI"
+	fmt.Println("Mark changed his speciality")
+	fmt.Println("His speciality is ", mark.speciality)
+	// 修改他的年龄信息
+	fmt.Println("Mark become old")
+	mark.age = 46 //修改也方便了
+	fmt.Println("His age is", mark.age)
+	// 修改他的体重信息
+	fmt.Println("Mark is not an athlet anymore")
+	mark.weight += 60
+	fmt.Println("His weight is", mark.weight)
+}
