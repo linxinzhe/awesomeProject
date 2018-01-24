6f55b4110417d3b22fb94d45832a35b06b72d149
linxinzhe <linxinzhechn@icloud.com>
2018-01-24 16:19:14 +0800

    feat(debug): print caller file and number and print all current stack

--- stringutils/re.go
+++ stringutils/re.go
@@ -1,9 +1,10 @@
-package strings
+package stringutils
 
 import (
 	"regexp"
 )
 
+// FindStringExample find the string by using regex
 func FindStringExample() string {
 	str := "abcd1234"
 	r := regexp.MustCompile(`[0-9]+`)
---
+++ util/debug.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+	"runtime/debug"
+	"runtime"
+	"os"
+)
+
+// Report prints caller file and number and prints all current stack
+// Report gives off a warning requesting the user to submit an issue to the github tracker.
+func Report(extra ...interface{}) {
+	fmt.Fprintln(os.Stderr, "You've encountered a sought after, hard to reproduce bug. Please report this to the developers <3 https://github.com/linxinzhe/go-codebase/issues")
+	fmt.Fprintln(os.Stderr, extra...)
+
+	_, file, line, _ := runtime.Caller(1)         //lxz 跳过Report自身，打印调用Report的函数的堆栈的行和文件
+	fmt.Fprintf(os.Stderr, "%v:%v\n", file, line) //lxz F表示自行决定打印的输出流，printf表示按参数打印
+
+	debug.PrintStack() //lxz 打印所有的内存堆栈
+
+	fmt.Fprintln(os.Stderr, "#### BUG! PLEASE REPORT ####")
+}
