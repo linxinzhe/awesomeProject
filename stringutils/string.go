03779c1fca6700a01aeec3da0b28234533912d06
linxinzhe <linxinzhechn@icloud.com>
2018-01-25 13:58:43 +0800

    feat(string): lengthOfUTF8

--- stringutils/string.go
+++ stringutils/string.go
@@ -3,6 +3,7 @@ package stringutils
 import (
 	"strings"
 	"fmt"
+	"unicode/utf8"
 )
 
 // Contains check if str is in text
@@ -10,7 +11,13 @@ func Contains(text string, str string) bool {
 	return strings.Contains(text, str)
 }
 
-func StringToByte() {
+func stringToByte() {
 	a := "啊bcd" // utf-8，一个中文顶三个byte，一个英文一个byte
 	fmt.Println([]byte(a))
+}
+
+func lengthOfUTF8() {
+	s := "Hello, 世界"
+	fmt.Println(len(s))                    // "13"
+	fmt.Println(utf8.RuneCountInString(s)) // "9"
 }
