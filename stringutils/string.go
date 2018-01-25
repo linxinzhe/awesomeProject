1fbd88f9d23a1952a23e7266062221f8440af07a
linxinzhe <linxinzhechn@icloud.com>
2018-01-25 10:42:08 +0800

    feat(string): string to byte, utf-8: 1 chinese = 3 byte, 1 english = 1 byte

--- stringutils/string.go
+++ stringutils/string.go
@@ -2,9 +2,15 @@ package stringutils
 
 import (
 	"strings"
+	"fmt"
 )
 
 // Contains check if str is in text
 func Contains(text string, str string) bool {
 	return strings.Contains(text, str)
+}
+
+func StringToByte() {
+	a := "啊bcd" // utf-8，一个中文顶三个byte，一个英文一个byte
+	fmt.Println([]byte(a))
 }
