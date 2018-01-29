3e1a561a74deda8245e36e98d36f580d8c9f7f30
linxinzhe <linxinzhechn@icloud.com>
2018-01-29 16:31:08 +0800

    feat(network): http get

---
+++ network/http_get.go
@@ -0,0 +1,24 @@
+package network
+
+import (
+	"net/http"
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+func HttpGet() {
+	url := ""
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch:%v\n", err)
+		os.Exit(1)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", b)
+}
